c236c30a3dc0d84c7bee8ace5a460a117b5f5d4e
Hays <huangwenxi@cvte.com>
2017-12-23 12:04:17 +0800

    update base syntax

--- app/src/main/java/com/hays/kotlinlearning/synopsis/BaseSyntax.kt
+++ app/src/main/java/com/hays/kotlinlearning/synopsis/BaseSyntax.kt
@@ -4,3 +4,33 @@ package com.hays.kotlinlearning.synopsis
  * @author Hays
  * @date 23/12/2017
  */
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun sum2(a: Int, b: Int) = a + b
+
+var x = 0
+fun autoIncrease() {
+    x++
+}
+
+fun checkString(obj: Any): Boolean {
+    return (obj is String)
+}
+
+fun checkIsNotString(obj: Any): Boolean {
+    return (obj !is String)
+}
+
+fun useRange(maxSize: Int): Int {
+    var size = maxSize
+    if (maxSize !in 1..10) {
+        size = 10
+    }
+    var ret = 0
+    for (i in 1..size) {
+        ret += i*10
+    }
+    return ret
+}
--- app/src/test/java/com/hays/kotlinlearning/synopsis/BaseSyntaxTest.kt
+++ app/src/test/java/com/hays/kotlinlearning/synopsis/BaseSyntaxTest.kt
@@ -1,6 +1,42 @@
 package com.hays.kotlinlearning.synopsis
 
+import mockit.integration.junit4.JMockit
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
 /**
  * @author Hays
  * @date 23/12/2017
  */
+@RunWith(JMockit::class)
+class BaseSyntaxTest {
+
+    @Test
+    fun testSumFunc() {
+        var ret = sum(1, 2)
+        assertEquals(3, ret)
+        ret = sum2(1, 2 )
+        assertEquals(3, ret)
+    }
+
+    @Test
+    fun testIncrease() {
+        for (i in 1..3) {
+            autoIncrease()
+        }
+        assertEquals(3, x)
+    }
+
+    @Test
+    fun testCheckString() {
+        assertEquals(true, checkString("123"))
+        assertEquals(false, checkString(123))
+    }
+
+    @Test
+    fun testCheckNotString() {
+        assertEquals(true, checkIsNotString(123))
+        assertEquals(false, checkIsNotString("123"))
+    }
+}
